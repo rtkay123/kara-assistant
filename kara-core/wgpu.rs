09c6ce4abb5ffdd6427fc707e0c3cdc4ebcd5979
Rodney Kanjala <dev@kanjala.com>
2022-04-26 14:30:03 +0200

    feat: add iced controls

--- kara-core/wgpu.rs
+++ kara-core/wgpu.rs
@@ -1,4 +1,67 @@
-pub mod scene {
+mod controls {
+    use iced_wgpu::Renderer;
+    use iced_winit::{
+        widget::{Column, Container, Row, Text},
+        Color, Program,
+    };
+
+    pub struct Controls {
+        background_color: Color,
+        text: String,
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum Message {
+        TextChanged(String),
+    }
+
+    impl Controls {
+        pub fn new() -> Self {
+            Self {
+                background_color: Color {
+                    r: 0.4,
+                    g: 0.4,
+                    b: 0.4,
+                    a: 1.0,
+                },
+                text: String::from("Hey there"),
+            }
+        }
+
+        pub fn background_color(&self) -> Color {
+            self.background_color
+        }
+    }
+
+    impl Program for Controls {
+        type Renderer = Renderer;
+
+        type Message = Message;
+
+        fn update(&mut self, message: Self::Message) -> iced_winit::Command<Self::Message> {
+            match message {
+                Message::TextChanged(val) => self.text = val,
+            }
+            iced_winit::Command::none()
+        }
+
+        fn view(&mut self) -> iced_winit::Element<'_, Self::Message, Self::Renderer> {
+            let content = Row::new()
+                .width(iced_winit::Length::Fill)
+                .height(iced_winit::Length::Fill)
+                .push(Column::new().width(iced_winit::Length::Fill).push(
+                    Text::new(&self.text).color(Color::new(
+                        0.949_019_6,
+                        0.898_039_2,
+                        0.737_254_9,
+                        1.0,
+                    )),
+                ));
+            Container::new(content).into()
+        }
+    }
+}
+mod scene {
     use iced_wgpu::wgpu;
 
     pub struct Scene {
