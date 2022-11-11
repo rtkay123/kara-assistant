464b34c939001c814740db836084cc6ff4f6ca42
kawaki-san <dev@kanjala.com>
2022-11-12 00:12:03 +0200

    fix: stop application when cli session ends

--- crates/kara/src/main.rs
+++ crates/kara/src/main.rs
@@ -12,19 +12,19 @@ fn main() {
 #[tokio::main]
 async fn start() {
     let args = config::initialise_application();
-    if cfg!(all(feature = "graphical", feature = "commandline")) {
-        match args.mode.unwrap_or_default() {
-            config::cli::StartupMode::Gui => {
-                graphics::run().await.unwrap();
-            }
-            config::cli::StartupMode::Cli => {
-                println!("starting cli");
-            }
+    #[cfg(all(feature = "graphical", feature = "commandline"))]
+    match args.mode.unwrap_or_default() {
+        config::cli::StartupMode::Gui => {
+            graphics::run().await.unwrap();
         }
-    } else if cfg!(not(feature = "graphical")) && cfg!(feature = "commandline") {
-        println!("starting cli");
-    } else if cfg!(not(feature = "commandline")) && cfg!(feature = "graphical") {
-        #[cfg(feature = "graphical")]
-        graphics::run().await.unwrap();
+        config::cli::StartupMode::Cli => {
+            println!("starting cli");
+            std::process::exit(0);
+        }
     }
+
+    #[cfg(feature = "graphical")]
+    graphics::run().await.unwrap();
+    #[cfg(feature = "commandline")]
+    println!("starting cli");
 }
