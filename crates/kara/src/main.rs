0109b16bb314232a3f0c5bacabbff7255e6a5c7a
rtkay <dev@kanjala.com>
2023-06-24 13:14:47 +0200

    feat: add logging via tracing

--- crates/kara/src/main.rs
+++ crates/kara/src/main.rs
@@ -1,3 +1,19 @@
+use tracing::info;
+
 fn main() {
-    println!("Hello, world!");
+    init_logger();
+}
+
+fn init_logger() {
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "kara=debug,other_crate=info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let pkg_name = env!("CARGO_PKG_NAME");
+    let pkg_ver = env!("CARGO_PKG_VERSION");
+    info!(version = pkg_ver, "{} has started", pkg_name);
 }
